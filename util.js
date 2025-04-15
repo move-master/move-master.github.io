41e4ad02744b2571ed1eb8d15dce8f48cb37c9ef
KAALMOMA <katrinamosquera@gmail.com>
2025-04-15 13:26:22 -0400

    Cleaned Up Graphics + Added GSSV2

--- util.js
+++ util.js
@@ -1,136 +1,96 @@
-$(document).ready(function(){
-   $(".overview-button").addClass("link");
-   $("#overview_page").show();
-   $("#mtgm_page").hide();
-   $("#gsvis_page").hide();
-   $("#gssv1_page").hide();
-   $("#rour_page").hide();
-   $("#gsgip_page").hide();
-   $("#gssv2_page").hide();
+$(document).ready(function () {
+  function saveActiveSection(sectionId) {
+    localStorage.setItem("activeSection", sectionId);
+  }
 
-
-   $(".overview-button").click(function(){
-      $(".overview-button").addClass("link");
-      $(".u-24-mtgm-button").removeClass("link")
-      $(".u-24-gsvis-button").removeClass("link")
-      $(".u-24-gssv-1-button").removeClass("link")
-      $(".s-25-rour-button").removeClass("link")
-      $(".s-25-gsgip-button").removeClass("link")
-      $(".s-25-gssv2-button").removeClass("link")
-      $("#overview_page").show();
-      $("#mtgm_page").hide();
-      $("#gsvis_page").hide();
-      $("#gssv1_page").hide();
-      $("#rour_page").hide();
-      $("#gsgip_page").hide();
-      $("#gssv2_page").hide();
+  function showSection(sectionId) {
+    const allSections = [
+      "#overview_page",
+      "#mtgm_page",
+      "#gsvis_page",
+      "#gssv1_page",
+      "#rour_page",
+      "#gsgip_page",
+      "#gssv2_page"
+    ];
+    allSections.forEach(section => {
+      if (section === sectionId) {
+        $(section).fadeIn(500);
+      } else {
+        $(section).hide();
+      }
     });
+    saveActiveSection(sectionId);
+  }
 
-
-   $(".u-24-mtgm-button").click(function(){
-      $(".overview-button").removeClass("link");
-      $(".u-24-mtgm-button").addClass("link")
-      $(".u-24-gsvis-button").removeClass("link")
-      $(".u-24-gssv-1-button").removeClass("link")
-      $(".s-25-rour-button").removeClass("link")
-      $(".s-25-gsgip-button").removeClass("link")
-      $(".s-25-gssv2-button").removeClass("link")
-      $("#overview_page").hide();
-      $("#mtgm_page").show();
-      $("#gsvis_page").hide();
-      $("#gssv1_page").hide();
-      $("#rour_page").hide();
-      $("#gsgip_page").hide();
-      $("#gssv2_page").hide();
-   });
-
-
-   $(".u-24-gsvis-button").click(function(){
-      $(".overview-button").removeClass("link");
-      $(".u-24-mtgm-button").removeClass("link")
-      $(".u-24-gsvis-button").addClass("link")
-      $(".u-24-gssv-1-button").removeClass("link")
-      $(".s-25-rour-button").removeClass("link")
-      $(".s-25-gsgip-button").removeClass("link")
-      $(".s-25-gssv2-button").removeClass("link")
-      $("#overview_page").hide();
-      $("#mtgm_page").hide();
-      $("#gsvis_page").show();
-      $("#gssv1_page").hide();
-      $("#rour_page").hide();
-      $("#gsgip_page").hide();
-      $("#gssv2_page").hide();
+  function highlightButton(buttonClass) {
+    const allButtons = [
+      ".overview-button",
+      ".u-24-mtgm-button",
+      ".u-24-gsvis-button",
+      ".u-24-gssv-1-button",
+      ".s-25-rour-button",
+      ".s-25-gsgip-button",
+      ".s-25-gssv-2-button"
+    ];
+    allButtons.forEach(btn => {
+      if (btn === buttonClass) {
+        $(btn).addClass("link");
+      } else {
+        $(btn).removeClass("link");
+      }
     });
+  }
 
+  // Restore saved section on page load or default to overview
+  const savedSection = localStorage.getItem("activeSection") || "#overview_page";
+  showSection(savedSection);
 
-    $(".u-24-gssv-1-button").click(function(){
-      $(".overview-button").removeClass("link");
-      $(".u-24-mtgm-button").removeClass("link")
-      $(".u-24-gsvis-button").removeClass("link")
-      $(".u-24-gssv-1-button").addClass("link")
-      $(".s-25-rour-button").removeClass("link")
-      $(".s-25-gsgip-button").removeClass("link")
-      $(".s-25-gssv2-button").removeClass("link")
-      $("#overview_page").hide();
-      $("#mtgm_page").hide();
-      $("#gsvis_page").hide();
-      $("#gssv1_page").show();
-      $("#rour_page").hide();
-      $("#gsgip_page").hide();
-      $("#gssv2_page").hide();
-    });
+  // Also restore the correct button highlight
+  const sectionToButton = {
+    "#overview_page": ".overview-button",
+    "#mtgm_page": ".u-24-mtgm-button",
+    "#gsvis_page": ".u-24-gsvis-button",
+    "#gssv1_page": ".u-24-gssv-1-button",
+    "#rour_page": ".s-25-rour-button",
+    "#gsgip_page": ".s-25-gsgip-button",
+    "#gssv2_page": ".s-25-gssv-2-button"
+  };
+  highlightButton(sectionToButton[savedSection]);
 
+  // Set up click listeners
+  $(".overview-button").click(function () {
+    highlightButton(".overview-button");
+    showSection("#overview_page");
+  });
 
-    $(".s-25-rour-button").click(function(){
-      $(".overview-button").removeClass("link");
-      $(".u-24-mtgm-button").removeClass("link")
-      $(".u-24-gsvis-button").removeClass("link")
-      $(".u-24-gssv-1-button").removeClass("link")
-      $(".s-25-rour-button").addClass("link")
-      $(".s-25-gsgip-button").removeClass("link")
-      $(".s-25-gssv2-button").removeClass("link")
-      $("#overview_page").hide();
-      $("#mtgm_page").hide();
-      $("#gsvis_page").hide();
-      $("#gssv1_page").hide();
-      $("#rour_page").show();
-      $("#gsgip_page").hide();
-      $("#gssv2_page").hide();
-    });
+  $(".u-24-mtgm-button").click(function () {
+    highlightButton(".u-24-mtgm-button");
+    showSection("#mtgm_page");
+  });
 
+  $(".u-24-gsvis-button").click(function () {
+    highlightButton(".u-24-gsvis-button");
+    showSection("#gsvis_page");
+  });
 
-    $(".s-25-gsgip-button").click(function(){
-      $(".overview-button").removeClass("link");
-      $(".u-24-mtgm-button").removeClass("link")
-      $(".u-24-gsvis-button").removeClass("link")
-      $(".u-24-gssv-1-button").removeClass("link")
-      $(".s-25-rour-button").removeClass("link")
-      $(".s-25-gsgip-button").addClass("link")
-      $(".s-25-gssv2-button").removeClass("link")
-      $("#overview_page").hide();
-      $("#mtgm_page").hide();
-      $("#gsvis_page").hide();
-      $("#gssv1_page").hide();
-      $("#rour_page").hide();
-      $("#gsgip_page").show();
-      $("#gssv2_page").hide();
-    });
+  $(".u-24-gssv-1-button").click(function () {
+    highlightButton(".u-24-gssv-1-button");
+    showSection("#gssv1_page");
+  });
 
+  $(".s-25-rour-button").click(function () {
+    highlightButton(".s-25-rour-button");
+    showSection("#rour_page");
+  });
 
-    $(".s-25-gssv2-button").click(function(){
-      $(".overview-button").removeClass("link");
-      $(".u-24-mtgm-button").removeClass("link")
-      $(".u-24-gsvis-button").removeClass("link")
-      $(".u-24-gssv-1-button").removeClass("link")
-      $(".s-25-rour-button").removeClass("link")
-      $(".s-25-gsgip-button").removeClass("link")
-      $(".s-25-gssv2-button").addClass("link")
-      $("#overview_page").hide();
-      $("#mtgm_page").hide();
-      $("#gsvis_page").hide();
-      $("#gssv1_page").hide();
-      $("#rour_page").hide();
-      $("#gsgip_page").hide();
-      $("#gssv2_page").show();
-    });
+  $(".s-25-gsgip-button").click(function () {
+    highlightButton(".s-25-gsgip-button");
+    showSection("#gsgip_page");
+  });
+
+  $(".s-25-gssv-2-button").click(function () {
+    highlightButton(".s-25-gssv-2-button");
+    showSection("#gssv2_page");
+  });
 });
