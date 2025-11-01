0f4ee1fd1cbd65ce4b26022181a1fbe9c1e5eb72
Katrina Mosquera-Maykovych <katrinamosquera@gmail.com>
2025-10-31 21:13:04 -0400

    Working on docs site v2

---
+++ header.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.getElementById('header');
+    if (header) {
+        header.innerHTML = 
+        "<div class=\"header\"> \
+            <img src=\"resources/mm-icon.png\" height=50> \
+            <img src=\"resources/mm-logo.png\" height=35> \
+            <h1>Documentation</h1> \
+            <div class=\"vbar\"></div> \
+            <a href=\"https://www.example.com/\">About Us</a> \
+            <div class=\"vbar\"></div> \
+            <a href=\"https://www.example.com/\">View our Project on rcos.io</a> \
+            <div class=\"vbar\"></div> \
+            <a href=\"https://www.example.com/\">View our Repos on GitHub</a> \
+        </div>";
+    }
+
+    const nav = document.getElementById('nav');
+    if (nav) {
+        nav.innerHTML = 
+        "<div class=\"nav\"> \
+            <h1>Menu</h1> \
+            <a href=\"https://www.example.com/\">About Us</a> \
+            <a href=\"https://www.example.com/\">View our Project on rcos.io</a> \
+            <a href=\"https://www.example.com/\">View our Repos on GitHub</a> \
+            <a href=\"https://www.example.com/\">View our Repos on GitHub</a> \
+        </div>";
+    }
+});
